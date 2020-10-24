08bd90416269724a1368bea99394d4b6ea741b8d
nelliez P <nook0019@gmail.com>
2020-10-24 17:13:13 +0700

    shift and fib

---
+++ index.js
@@ -0,0 +1,35 @@
+function fib(n) {
+    let fst = 1;
+    let snd = 1;
+  
+    if (n === 1 || n === 2) {
+      return 1;
+    }
+  
+    let result;
+    for (let i = 2; i < n; i++) {
+      result = fst + snd;
+      fst = snd; 
+      snd = result; 
+    }
+  
+    return result;
+  }
+
+  function shift(arr, dir, n) {
+    const result = [];
+  
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      if (dir === "right") {
+        result[(i + n) % len] = arr[i];
+      } else {
+        const tmp = i - n;
+        const index = tmp < 0 ? tmp + len : tmp;
+        result[index] = arr[i];
+      }
+    }
+  
+    return result;
+  }
+  
