7efe303c16e431cbcfa4ca92843e3e8992013976
nelliez P <nook0019@gmail.com>
2020-10-26 12:15:07 +0700

    fix

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ function fib(n) {
     return result;
   }
 
-//   fib(12);s
+console.log("fib > ",fib(1));
+console.log("fib > ",fib(3));
+console.log("fib > ",fib(12));
 
   function shift(arr, dir, n) {
     const result = [];
-  
+    
     const len = arr.length;
     for (let i = 0; i < len; i++) {
       if (dir === "right") {
@@ -33,7 +35,39 @@ function fib(n) {
         
       }
     }
-  
+    
     return result;
   }
   
+  // shift(arr,right,3);
+  console.log("shift > ",shift(['nook','kohpai','jane','nell','Aliz'],'right',0));
+  console.log("shift > ",shift(['nook','kohpai','jane','nell','Aliz'],'right',3));
+  console.log("shift > ",shift(['nook','kohpai','jane','nell','Aliz'],'left',3));
+
+  
+  function secondMax(arr = []) {
+    let maxArr = arr[0];
+    let nextmaxArr = arr[0];
+  
+    if (arr.length === 0) {
+      return "Error!";
+    }
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > maxArr) {
+        nextmaxArr = maxArr;
+        maxArr = arr[i];
+      } else if (arr[i] > nextmaxArr && arr[i] != maxArr) {
+        nextmaxArr = arr[i];
+      }
+    }
+  
+    return nextmaxArr;
+  }
+  
+  
+  console.log("secondMax >",secondMax([]));
+  console.log("secondMax >",secondMax([2, 3, 4, 5]));
+  console.log("secondMax >",secondMax([9, 2, 21, 21]));
+  console.log("secondMax >",secondMax([4123]));
+  console.log("secondMax >",secondMax([4,4,4,4]));
